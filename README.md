76d2093a7f27793964dddac71d7bb8d40899adc3
DrewRic <andrewricker1@gmail.com>
2024-09-13 18:35:40 -0400

    Readme Updated

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # Thought-Blog
+
+# Description
+
+This project is a NoSQL Challenge where we build a RESTful API for a social network web application using MongoDB, Express.js, and Mongoose. The application allows users to share thoughts, react to friends' thoughts, and manage a friend list. MongoDB is used for its scalability and ability to handle unstructured data, which is essential for social networking platforms that process large amounts of dynamic user content.
+
+Since this project focuses on backend functionality, the app is not deployed. Instead, interactions with the API can be tested using tools like Insomnia or Postman. The project's functionality is demonstrated through a walkthrough video showcasing how each API route works.
+
+# Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies Used](#technologies-used)
+- [API Routes](#api-routes)
+- [License](#license)
+
+# Installation
+1. Clone the repository: https://github.com/DrewRic/Thought-Blog.git
+2. Install dependencies
+    -npm install i
+3. Ensure that MongoDB is installed and running on your machine.
+4. Run the application
+
+# Usage
+
+Once the server is running, you can use Insomnia or Postman to interact with the API by testing its various routes.
+
+To create a user, send a POST request to /api/users with the appropriate JSON data.
+To view all users, send a GET request to /api/users.
+
+This API allows CRUD operations (Create, Read, Update, Delete) on users, thoughts, reactions, and friends.
+
+# Technologies Used
+
+MongoDB: A NoSQL database for storing unstructured data.
+Mongoose: An ODM (Object Data Modeling) library to interact with MongoDB.
+Express.js: A framework for building the API and handling routes.
+Node.js: JavaScript runtime for running the application backend.
+
+# API Routes
+
+Users
+    GET /api/users: Get all users.
+    GET /api/users/
+    : Get a single user by ID.
+    POST /api/users: Create a new user.
+    PUT /api/users/
+    : Update an existing user.
+    DELETE /api/users/
+    : Delete a user.
+
+Thoughts
+    GET /api/thoughts: Get all thoughts.
+    GET /api/thoughts/
+    : Get a single thought by ID.
+    POST /api/thoughts: Create a new thought.
+    PUT /api/thoughts/
+    : Update an existing thought.
+    DELETE /api/thoughts/
+    : Delete a thought.
+
+Reactions
+    POST /api/thoughts/
+    /reactions: Add a reaction to a thought.
+    DELETE /api/thoughts/
+    /reactions/
+    : Remove a reaction from a thought.
+
+Friends
+    POST /api/users/
+    /friends/
+    : Add a friend to a user's friend list.
+    DELETE /api/users/
+    /friends/
+    : Remove a friend from a user's friend list.
+
+# License
+This project is licensed under the MIT License.
--- utils/data.js
+++ utils/data.js
@@ -1,46 +1,71 @@
 const names = [
-    "Aaron",
-    "Brandon",
-    "Smith",
-    "Jones",
-    "Brian",
-    "Heather",
-    "Kenneth",
-    "Xander",
-    "Jared",
-    "Courtney",
- 
-  ];
-  
-  const thoughtContent = [
-    "I want to be free",
-    "I miss my hobbies",
-    "I have no time to myself anymore",
-    "There needs to be more vietnamese restaurants around here",
-    "I miss playing video games",
-    "I need to stop shopping so much",
-    "I want to make projcets that are meaningful to me",
-    "Are you we doing okay?",
-  ];
-  
-  // Get a random item given an array
-  const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
-  
-  // Gets a random full name
-  const getRandomName = () =>
-    `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
-  
-  // Function to generate random assignments that we can add to student object.
-  const getRandomThoughts = (int) => {
-    const results = [];
-    for (let i = 0; i < int; i++) {
-      results.push({
-        thoughtName: getRandomArrItem(thoughtContent),
-        score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-      });
-    }
-    return results;
-  };
-  
-  // Export the functions for use in seed.js
-  module.exports = { getRandomName, getRandomThoughts };
+  "Shannon",
+  "Kortney",
+  "Aarez",
+  "Michael",
+  "Aaron",
+  "Brandon",
+  "Smith",
+  "Jones",
+  "Brian",
+  "Heather",
+  "Kenneth",
+  "Xander",
+  "Jared",
+  "Courtney",
+  "Gillian",
+  "Clark",
+  "Grace",
+  "Kelsey",
+  "Tamar",
+  "Alex",
+  "Mark",
+  "Ryan",
+  "Andrew",
+  "Sarah",
+  "Nathaniel",
+  "Parker",
+];
+
+const thoughtContent = [
+  "I want to be free",
+  "I miss my hobbies",
+  "I have no time to myself anymore",
+  "There needs to be more vietnamese restaurants around here",
+  "I want more cats",
+  "I want more dogs",
+  "I miss playing video games",
+  "I need to stop shopping so much",
+  "I want to make projcets that are meaningful to me",
+  "Are you we doing okay?",
+  "I want to go hiking",
+  "What is the plan today?",
+  "When are we going to go hiking?",
+  "I want to do ceramics",
+  "I can't believe I was lucky enough!",
+  "Are we really going to pretend that did not happen?",
+  "I need to make a grocery list.",
+  "I am craving sushi",
+];
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets a random full name
+const getRandomName = () =>
+  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+// Function to generate random assignments that we can add to student object.
+const getRandomThoughts = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughtName: getRandomArrItem(thoughtContent),
+      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+    });
+  }
+  return results;
+};
+
+// Export the functions for use in seed.js
+module.exports = { getRandomName, getRandomThoughts };
